e309248eafe810f0f02653cfb1336296e2bb7066
Shaun <shaun@Shauns-Air.lan>
2022-07-12 14:50:22 +1200

    some initial components

---
+++ src/components/MinesweeperBoard.js
@@ -0,0 +1,7 @@
+import { useState } from "react";
+
+function MinesweeperBoard() {
+  return <div>Minesweeper Board</div>;
+}
+
+export default MinesweeperBoard;
---
+++ src/pages/MinesweeperPage.js
@@ -0,0 +1,9 @@
+function MinesweeperPage() {
+  return (
+    <div>
+      <h1>Minesweeper</h1>
+    </div>
+  );
+}
+
+export default MinesweeperPage;
