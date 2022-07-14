dcd51c9782d84e167a3f61a770a3ea793009ec84
Shaun <shaun@Shauns-Air.lan>
2022-07-14 16:32:05 +1200

    minses are blue :)

--- src/components/GridBlock.js
+++ src/components/GridBlock.js
@@ -2,10 +2,21 @@ function GridBlock(props) {
   return (
     <div>
       <button
-        type="button"
-        className="static focus:outline-none bg-slate-300 hover:bg-slate-400 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
+        className={`static focus:outline-none 
+          ${props.item.clicked === false ? "bg-slate-300" : "bg-red-300"} 
+          ${
+            props.item.clicked === false || props.neighbourMines === 0
+              ? "text-transparent"
+              : "text-black"
+          }
+          ${props.item.mine === false ? "bg-slate-300" : "bg-blue-300"}
+          font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2`}
+        key={props.index}
+        item={props.item}
+        onClick={(event) => props.handleClick(event, props.index)}
       >
-        {props.item.column} - {props.item.row}
+        {" "}
+        {props.item.neighbourMines}
       </button>
     </div>
   );
--- src/components/MinesweeperBoard.js
+++ src/components/MinesweeperBoard.js
@@ -1,16 +1,45 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import GridBlock from "./GridBlock";
 
 function MinesweeperBoard() {
   let items = Array.from(Array(100).keys()).map((item, index) => {
     const column = index % 10;
     const row = Math.floor(index / 10);
-    return { column, row, clicked: false };
+    return {
+      column,
+      row,
+      clicked: false,
+      mine: false,
+      flag: false,
+      neighbourMines: 0,
+    };
   });
 
   const [boardState, setBoardState] = useState({ items });
 
-  console.log(boardState);
+  function populateMines() {
+    const newBoard = boardState.items.map((item, index) => {
+      let randNum = Math.random();
+      if (randNum < 0.2) {
+        return {
+          column: item.column,
+          row: item.row,
+          clicked: false,
+          mine: true,
+          flag: false,
+          neighbourMines: 0,
+        };
+      } else {
+        return item;
+      }
+    });
+    setBoardState({ items: newBoard });
+    console.log(newBoard);
+  }
+
+  useEffect(() => {
+    populateMines();
+  }, []);
 
   const handleClick = (event, index) => {
     const oldBoard = boardState.items;
@@ -19,16 +48,25 @@ function MinesweeperBoard() {
   };
 
   return (
-    <div className="grid grid-cols-10 gap-0 w-1/2">
+    <div className="grid grid-cols-10 gap-0 w-1/2 bg-slate-400 pt-2 pl-2">
       {boardState.items.map((item, index) => (
         <button
-          className={`static focus:outline-none ${
-            item.clicked === false ? "bg-slate-300" : "bg-red-300"
-          } font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2`}
+          className={`static focus:outline-none 
+          ${item.clicked === false ? "bg-slate-300" : "bg-red-300"} 
+          ${
+            item.clicked === false || item.neighbourMines === 0
+              ? "text-transparent"
+              : "text-black"
+          }
+          ${item.mine === false ? "bg-slate-300" : "bg-blue-300"}
+          font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2`}
           key={index}
           item={item}
           onClick={(event) => handleClick(event, index)}
-        ></button>
+        >
+          {" "}
+          {item.neighbourMines}
+        </button>
       ))}
     </div>
   );
